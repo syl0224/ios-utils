7c8b9fe6e1348e933dcb2eeef155ae8d72314e8a
sunyanling <sunyanling@zhihu.com>
2019-09-02 12:30:20 +0800

    support iPhone X or above

--- test/ios-utils.test.js
+++ test/ios-utils.test.js
@@ -2,6 +2,7 @@
 
 const iOSUtils = require('..');
 const assert = require('assert');
+const _ = require('../lib/ios-utils');
 
 describe('test', function() {
   it('should be ok', function *() {
@@ -14,5 +15,17 @@ describe('test', function() {
     var iOSSDKVersion = yield iOSUtils.getIOSSDKVersion();
     assert.ok(iOSSDKVersion);
     console.log(iOSSDKVersion);
+  });
+});
+
+describe('getDeviceInfo test', () => {
+  let res;
+  it('getDeviceInfo()', () => {
+    res = _.getDeviceInfo('00008020-001D4D38XXXXXXXX');
+    assert.equal(res.isIOS, true);
+    assert.equal(res.isRealIOS, true);
+    res = _.getDeviceInfo('B8B6B1F5-30E0-401B-B1CE-70E4F39937A5');
+    assert.equal(res.isIOS, true);
+    assert.equal(res.isRealIOS, false);
   });
 });
